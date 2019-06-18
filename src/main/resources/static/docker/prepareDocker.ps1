0b09f365f51655703c5f9ecd7cd3af9c3c0e4b13
berenar <bernatpericasserra97@gmail.com>
2019-06-18 14:05:35 +0200

    prepareDocker.ps1 runs docker if it's not running, when it has finished booting up, boots up the container, when it has finished setting up the container, starts Selenium server

--- src/main/resources/static/docker/prepareDocker.ps1
+++ src/main/resources/static/docker/prepareDocker.ps1
@@ -1,15 +1,38 @@
-﻿#Start Docker if it's not running
+﻿###################################################################################################################
+############################################## Run Docker and wait ################################################
+###################################################################################################################
+#Path of docker executable
 $Prog = "C:\Program Files\Docker\Docker\Docker for Windows.exe"
-$Running = Get-Process 'Docker for Windows' -ErrorAction SilentlyContinue
+#Command that starts it
 $Start = { ([wmiclass]"win32_process").Create($Prog) }
-if ($Running -eq $null)
-{
-    Write-Output "Starting Docker..."
+#To know if it's running
+$Running = Get-Process 'Docker for Windows' -ErrorAction SilentlyContinue
+#Start it if it's not running
+if($Running -eq $null){
+    echo "Docker starting ..."
+    echo "_______________________________"
     & $Start
-    Start-Sleep -s 60
-    Write-Output "Docker started"
+    echo "_______________________________"
+    #Wait 5 seconds to let Docker configure log files
+    Start-Sleep -s 5
 }
+#Get user username
+$uname = [Environment]::UserName;
+#Log file path
+$log_path="C:\Users\" + $uname +"\AppData\Local\Docker\log.txt"
+#Last log tells us if it has finished booting up everything
+$started = Get-Content $log_path | Select-String -Pattern 'Docker Desktop is running' -CaseSensitive -SimpleMatch
+while ($started -eq $null){
+    echo "..."
+    #Wait 5 more seconds
+    Start-Sleep -s 5
+    $started = Get-Content $log_path | Select-String -Pattern 'Docker Desktop is running' -CaseSensitive -SimpleMatch
+}
+echo "Docker is ready"
 
+###################################################################################################################
+############################################ Run Container and wait ###############################################
+###################################################################################################################
 #Get mapped folders paths (-v option)
 $down_path = "$( pwd )\src\main\resources\static\docker\mapped\Downloads:/home/user/Downloads"
 $desk_path = "$( pwd )\src\main\resources\static\docker\mapped\Desktop:/home/user/Desktop"
@@ -19,7 +42,8 @@ $desk_path = "$( pwd )\src\main\resources\static\docker\mapped\Desktop:/home/use
 If (-Not(docker ps | findstr "bernattt/ubuntubernat"))
 {
     #Run ubuntu container
-    Start-Job -Name run_job -ScriptBlock {docker run --privileged -p 6080:80 -p 6081:4444 --name ubuntubernat -v $args[0] -v $args[1] bernattt/ubuntubernat:v1 } -ArgumentList @($down_path,$desk_path);
+    Start-Job -Name run_job -ScriptBlock {
+        docker run --privileged -p 6080:80 -p 6081:4444 --name ubuntubernat -v $args[0] -v $args[1] bernattt/ubuntubernat:v1 } -ArgumentList @($down_path,$desk_path);
     #'docker run' output
     $run_output = Receive-Job -Name run_job 6>&1;
     #Wait until output of 'docker run' command says success (container started)
